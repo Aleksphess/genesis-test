1399e0caf758e05dd2ee30f428d5c569c240c9c3
Aleksphess <aleksphesspro@gmail.com>
2020-06-30 11:17:37 +0300

    Update init_test_db.sql

--- devops/mysql/init_test_db.sql
+++ devops/mysql/init_test_db.sql
@@ -1,4 +1,2 @@
 CREATE DATABASE IF NOT EXISTS genesis;
-GRANT ALL PRIVILEGES ON genesis.* TO 'root' identified by 'pass';
 CREATE DATABASE IF NOT EXISTS genesis_test;
-GRANT ALL PRIVILEGES ON genesis_test.* TO 'root' identified by 'pass';
